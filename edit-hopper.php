6dc87671a165e81a26008852eb26a67e786dfeba
Dylan Hildenbrand <dylanh@coderoadies.com>
2015-03-17 17:03:26 -0500

    Reworking some things

--- edit-hopper.php
+++ edit-hopper.php
@@ -26,66 +26,78 @@ Copyright 2013 Dylan Hildenbrand  (email : dylan.hildenbrand@gmail.com)
 
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-	function edithop_main(){
-	    echo "<div class='ultimate-container'>";
-			$current = get_post();
-			
-			$postargs = array(
-				'post_type' => $current->post_type,
-				'post_status' => 'publish',
-				'posts_per_page' => -1,
-				);
-			$postQuery = new WP_Query($postargs);
+define( 'EDIT_HOPPER_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-			if($postQuery->have_posts()) {
-				while ($postQuery->have_posts()) {
-					$postQuery->the_post();
-					if($current->ID == get_post()->ID) {
-						echo "<div class='edithop-link'>". get_the_title() . "</div>";
-					}
-					else {
-						echo "<div class='edithop-link'><a href='" . get_the_id() . "'>" . get_the_title() . "</a></div>";
+	class EditHopper {
+		
+		public function __construct() {
+
+		}
+
+		function init($screen) {
+			add_meta_box(
+		        'edit_hopper_box',
+		        __( 'Edit Relative Posts', 'edit_hopper_textdomain' ),
+		        array($this, 'edithop_main'),
+		        $screen
+		    );
+		}
+
+		function edithop_main(){
+		    echo "<div class='eh_ultimate_container'>";
+				$current = get_post();
+				
+				$postargs = array(
+					'post_type' => $current->post_type,
+					'post_status' => 'publish',
+					'posts_per_page' => -1,
+					'orderby' => 'menu_order',
+					'order' => 'DESC',
+					);
+				$postQuery = new WP_Query($postargs);
+
+				if($postQuery->have_posts()) {
+					while ($postQuery->have_posts()) {
+						$postQuery->the_post();
+						$linkPost = get_post();
+
+						// This post has no parent
+						if(empty($linkPost->post_parent)) {
+							if($current->ID == $linkPost->ID) {
+								echo "<div class='edithop-link'>". get_the_title() . "</div>";
+							}
+							else {
+								$ehpostlink = get_edit_post_link(get_the_id());
+								if(function_exists('wp_nonce_url')) {
+									wp_nonce_url($ehpostlink, 'edit-hopper-post-link');
+								}
+								echo "<div class='edithop-link'><a href='" . $ehpostlink . "'>" . get_the_title() . "</a></div>";
+							}
+						}
+						else {
+							if($current->ID == $linkPost->ID) {
+								echo "<div class='edithop-link'> - ". get_the_title() . "</div>";
+							}
+						}
 					}
 				}
-			}
-			wp_reset_query();
+				wp_reset_query();
 
-			/* Start Old and busted code here */
-			// $parvar = $current->post_parent;
-			// if($parvar) {
-			// 	echo "<div class='hopper-parent-link'><a href='" . get_edit_post_link($parvar) . "'>" 
-			// 	. get_post($parvar)->post_title . "</a></div>";
-			// }
-
-			// echo "<div class='hopper-current-link'>".get_post()->post_title. "</div>";
-
-
-			// $args = array(
-			// 			'post_type' => 'page',
-	  //       			'child_of' => (get_post()->ID));
-			// $children = get_pages($args);
-			// if($children)
-			// {
-			// 	foreach ($children as $child) {
-			// 		echo "<div class='hopper-child-link'> - <a href='" . get_edit_post_link($child->ID) . "'>" 
-			// 		. get_post($child->ID)->post_title . "</a></div>";
-			// 	}
-			// }
-
-	    echo "</div>";
+		    echo "</div>";
+		}
 	}
+
+	function eh_styles() {
+		wp_enqueue_style('hopper-style', EDIT_HOPPER_PLUGIN_URL .' css/hopper-style.css');
+	}
+	add_action('admin_init', 'eh_styles');
 
 	function edithop_custom_box() {
 		$screens = array( 'post', 'page' );
 
 		foreach ( $screens as $screen ) {
-
-		    add_meta_box(
-		        'edit_hopper_box',
-		        __( 'Edit Relative Posts', 'edit_hopper_textdomain' ),
-		        'edithop_main',
-		        $screen
-		    );
+			$hopper = new EditHopper();
+			$hopper->init($screen);
 		}
 	}
 	add_action( 'add_meta_boxes', 'edithop_custom_box' );
